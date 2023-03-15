1bed56d30d4be5e43404c6b4af0d651479cc17e7
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-03-15 21:34:58 +0900

    2023.03.15
    setState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let post = '역삼 우동 맛집';
+  // state = 자동 렌더링(자주 변경이 될 요소들)
+  let[글제목1, b] = useState('남자 코트 추천');
+  let[글제목2, c] = useState('강남 우동 맛집');
+  let[글제목3, d] = useState('파이썬 독학');
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{글제목1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목3}</h4>
+        <p>2월 17일 발행</p>
+      </div>           
     </div>
   );
 }
