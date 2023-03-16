0f72240951edadd2c8aa9538f293a455ae8d9e5b
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-03-16 22:04:06 +0900

    2023. 03. 16
    button(state사용)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -6,25 +7,37 @@ function App() {
 
   let post = '역삼 우동 맛집';
   // state = 자동 렌더링(자주 변경이 될 요소들)
-  let[글제목1, b] = useState('남자 코트 추천');
-  let[글제목2, c] = useState('강남 우동 맛집');
-  let[글제목3, d] = useState('파이썬 독학');
+  let[글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy = copy.sort();
+        글제목변경(copy);
+      }}>가나다순정렬</button>
+      <button onClick={() => {
+        // ... => 괄호를 벗겨달라는 뜻
+        let copy = [...글제목];
+        copy[0]= '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+      
       <div className="list">
-        <h4>{글제목1}</h4>
+        <h4>{글제목[0]}<span onClick={() => {따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목2}</h4>
+        <h4>{글제목[1]}<span>😛</span></h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목3}</h4>
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>           
     </div>
